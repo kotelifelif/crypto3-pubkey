9bd69a58989e52fa65a60bbf458f0bc9d5ad3429
nkaskov <nikitakaskov@mail.ru>
2020-05-09 06:33:40 -0400

    fixed some cmake problems #1

--- test/pkcs8.cpp
+++ test/pkcs8.cpp
@@ -1,3 +1,12 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2018-2019 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2020 Nikita Kaskov <nbering@nil.foundation>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//---------------------------------------------------------------------------//
+
 #include "fuzzers.h"
 
 #include <nil/crypto3/pubkey/pk_keys.hpp>
